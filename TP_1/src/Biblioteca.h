bf15f4853acf4c78bb2c1281e9b90ad07ab176de
LapMarcos <78131374+MarcosLaporte@users.noreply.github.com>
2021-09-25 13:50:57 -0300

    Corrección de documentación

--- TP_1/src/Biblioteca.h
+++ TP_1/src/Biblioteca.h
@@ -9,55 +9,55 @@
 /// @brief Pide un entero y lo valida.
 ///
 /// @param char[] - El primer mensaje a mostrar.
-/// @param char[] - El mensaje a mostrar en la validación.
-/// @param int - El parámetro de mínimo de la validación.
-/// @param int - El parámetro de máximo de la validación.
+/// @param char[] - El mensaje a mostrar en la validaciÃ³n.
+/// @param int - El parÃ¡metro de mÃ­nimo de la validaciÃ³n.
+/// @param int - El parÃ¡metro de mÃ¡ximo de la validaciÃ³n.
 /// @return int - El entero recibido.
 int PedirEntero(char[], char[], int, int);
 
-/// @brief Pide la opción a seleccionar del menú y la muestra en la consola.
+/// @brief Pide la opciÃ³n a seleccionar del menÃº y la muestra en la consola.
 ///
-/// @param float - Primer floatante ingresado, para mostrarlo en el menú.
-/// @param float - Segundo floatante ingresado, para mostrarlo en el menú.
-/// @param int - Una bandera para determinar si se ingresó o no el primer número a mostrar.
-/// @param int - Una bandera para determinar si se ingresó o no el segundo número a mostrar.
-/// @return int - El número de la opción elegida.
+/// @param float - Primer floatante ingresado, para mostrarlo en el menÃº.
+/// @param float - Segundo floatante ingresado, para mostrarlo en el menÃº.
+/// @param int - Una bandera para determinar si se ingresÃ³ o no el primer nÃºmero a mostrar.
+/// @param int - Una bandera para determinar si se ingresÃ³ o no el segundo nÃºmero a mostrar.
+/// @return int - El nÃºmero de la opciÃ³n elegida.
 int PedirOpcion(float, float, int, int);
 
 // Resultados------------------------------------------------
 
 /// @brief Muestra en consola el mensaje de resultado de suma.
 ///
-/// @param float - El primer número de la operación a mostrar.
-/// @param float - El segundo número de la operación a mostrar.
-/// @param float - El resultado de la operación a mostrar.
+/// @param float - El primer nÃºmero de la operaciÃ³n a mostrar.
+/// @param float - El segundo nÃºmero de la operaciÃ³n a mostrar.
+/// @param float - El resultado de la operaciÃ³n a mostrar.
 void ResultadoSuma(float, float, float);
 
 /// @brief Muestra en consola el mensaje de resultado de resta.
 ///
-/// @param float - El primer número de la operación a mostrar.
-/// @param float - El segundo número de la operación a mostrar.
-/// @param float - El resultado de la operación a mostrar.
+/// @param float - El primer nÃºmero de la operaciÃ³n a mostrar.
+/// @param float - El segundo nÃºmero de la operaciÃ³n a mostrar.
+/// @param float - El resultado de la operaciÃ³n a mostrar.
 void ResultadoResta(float, float, float);
 
-/// @brief Muestra en consola el mensaje de resultado de división.
+/// @brief Muestra en consola el mensaje de resultado de divisiÃ³n si es posible, sino, muestra mensaje de error.
 ///
-/// @param float - El primer número de la operación a mostrar.
-/// @param float - El segundo número de la operación a mostrar.
-/// @param float - El resultado de la operación a mostrar.
+/// @param float - El primer nÃºmero de la operaciÃ³n a mostrar.
+/// @param float - El segundo nÃºmero de la operaciÃ³n a mostrar.
+/// @param float - El resultado o error de la operaciÃ³n a mostrar.
 void ResultadoDivision(float, float, float);
 
-/// @brief Muestra en consola el mensaje de resultado de división si es posible, sino, muestra mensaje de error.
+/// @brief Muestra en consola el mensaje de resultado de multiplicaciÃ³n.
 ///
-/// @param float - El primer número de la operación a mostrar.
-/// @param float - El segundo número de la operación a mostrar.
-/// @param float - El resultado o error de la operación a mostrar.
+/// @param float - El primer nÃºmero de la operaciÃ³n a mostrar.
+/// @param float - El segundo nÃºmero de la operaciÃ³n a mostrar.
+/// @param float - El resultado de la operaciÃ³n a mostrar.
 void ResultadoMultiplicacion(float, float, float);
 
 /// @brief Muestra en consola el mensaje de resultado de factorial si es posible, sino, muestra mensaje de error.
 ///
-/// @param float - El número de la operación a mostrar.
-/// @param float - El resultado de la operación a mostrar.
+/// @param float - El nÃºmero de la operaciÃ³n a mostrar.
+/// @param float - El resultado de la operaciÃ³n a mostrar.
 void ResultadoFactorial(float, float);
 
 #endif /* BIBLIOTECA_H_ */
